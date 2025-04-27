2afb3c3ce34ae9b04f2c4d8c88520a5d5c5f2baa
Shiji Bijo <shijib.bijo@gmail.com>
2025-04-27 07:22:50 +0200

    Time: 10 ms (38.52%) | Memory: 59.4 MB (39.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,25 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (str) {
-  if (str.length === 0) return 0
-  const traverseString = (parentStr) => {
-    var charSet = new Set(parentStr[0]);
-    const strLen = parentStr.length;
-    let i = 1;
-    while (i < strLen) {
-      if (charSet.has(parentStr[i])) {
-        break;
-      } else charSet.add(parentStr[i]);
-      i++;
+var lengthOfLongestSubstring = function (s) {
+  let maxLen = 0;
+  let left = 0;
+  var charSet = new Set()
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
     }
-    return i;
+    charSet.add(s[right]);
+    maxLen = Math.max(maxLen, right - left + 1)
   }
-  let prevLen = 0;
-  for (let i = 0; i < str.length; i++) {
-    let childStr = str.substring(i);
-    currentSubLength = traverseString(childStr)
-    if (prevLen < currentSubLength) prevLen = currentSubLength
-  }
-  return prevLen;
+  return maxLen
 };
