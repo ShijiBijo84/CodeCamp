c065e0a7fd168f97c51983272b72f2cda0fad0c5
Shiji Bijo <shijib.bijo@gmail.com>
2025-04-28 12:58:42 +0200

    Time: 56 ms (27.96%) | Memory: 55.3 MB (63.14%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -5,7 +5,6 @@
 var reverse = function(x) {
   const minPow = -Math.pow(2, 31)
     const maxPow = Math.pow(2, 31) - 1
-    if (x > maxPow || x < minPow) return 0
     const sign = Math.sign(x)
     const intString = Math.abs(x).toString()
     let rev = intString.split('').reverse().join("")
